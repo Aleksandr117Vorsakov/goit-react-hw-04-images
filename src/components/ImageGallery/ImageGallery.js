142b854445bbb018ee29ff3c5868c4a2d6fee40f
Aleksandr117Vorsakov <vorsakov1994@gmail.com>
2022-10-16 22:02:36 +0300

    task hook

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { ImageGalleryItem } from '../ImageGalleryItem/ImageGalleryItem';
+import ImageGalleryItem from '../ImageGalleryItem';
+import { HITS_PER_PAGE } from '../../Services/fetchImages';
+
 import { Gallery } from './ImageGallery.styled';
 
 const ImageGallery = ({ items }) => {
+  useEffect(() => {
+    if (items.length <= HITS_PER_PAGE) {
+      return;
+    }
+    window.scrollBy({
+      top: 260 * 2,
+      behavior: 'smooth',
+    });
+  }, [items]);
+
   return (
     <Gallery>
       {items.map(item => (
